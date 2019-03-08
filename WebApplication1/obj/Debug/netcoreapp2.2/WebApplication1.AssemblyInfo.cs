b414b030623bcb82bef0b47afbddbc65af7e3f98
torimos <oleg.v.lytvynenko@gmail.com>
2019-03-09 00:00:24 +0200

    cleanup

--- WebApplication1/obj/Debug/netcoreapp2.2/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/netcoreapp2.2/WebApplication1.AssemblyInfo.cs
@@ -11,7 +11,6 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("d901bb07-8ece-44ec-8ff0-d0aacb4739ba")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
